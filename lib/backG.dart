b49d537278a5822f3e6e22b13fd86dc78f5e685f
Saad <saadm865@gmail.com>
2022-06-02 23:06:08 +0500

    Commit after assets

---
+++ lib/backG.dart
@@ -0,0 +1,93 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+//saada865/Domain-Finder
+
+class backG extends StatefulWidget {
+  const backG({Key? key}) : super(key: key);
+
+  @override
+  State<backG> createState() => _backGState();
+}
+
+class _backGState extends State<backG> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.deepPurpleAccent[100],
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0.0, 0.0, 280.0, 0.0),
+            child: Container(
+              height: 150,
+              width: 150,
+              //color: Colors.black,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/main_top.png"),
+                ),
+                //   color: Colors.green,
+              ),
+            ),
+          ),
+          Icon(
+            //Icons.add_moderator_sharp,
+            //Icons.add_task,
+            //Icons.add_to_home_screen,
+            //Icons.wysiwyg_rounded,
+            //Icons.workspace_premium_sharp,
+            //Icons.wrong_location_outlined,
+            Icons.zoom_in_rounded,
+            size: 125,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          Card(
+            color: Colors.deepPurpleAccent[100],
+            shadowColor: Colors.transparent,
+            child: Padding(
+                padding: const EdgeInsets.fromLTRB(75.0, 10, 20, 0),
+                child:
+                    // Card(
+                    //color: Color.fromARGB(255, 60, 17, 82),
+                    // child:
+                    Text(
+                  "Welcome To Domain Finder!",
+                  //style: //TextStyle(
+                  //color: Color.fromARGB(255, 140, 14, 203),
+                  style: GoogleFonts.bebasNeue(
+                      fontStyle: FontStyle.normal, fontSize: 50),
+
+                  //fontSize: 38,
+                  // ),
+                  //),
+                )),
+          ),
+          SizedBox(
+            height: 100,
+          ),
+          Container(
+              height: 70,
+              width: 220,
+              decoration: BoxDecoration(
+                  color: Colors.grey[200],
+                  borderRadius: BorderRadius.circular(10)),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Press to get Domain Name",
+                  style: TextStyle(
+                    color: Colors.purple[300],
+                    fontSize: 15,
+                    //backgroundColor: Colors.purple
+                  ),
+                ),
+              ))
+        ],
+      ),
+    );
+  }
+}
